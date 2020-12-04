957bca8029c1aeeae0f86ce679c955b4b14747d7
TuxSH <1922548+TuxSH@users.noreply.github.com>
2020-12-04 01:22:51 +0000

    Update web pages
    - Add new configurations
    - Only show relevant system versions
    - Current year is 2020 and the New 2DS XL is a thing

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,23 +5,42 @@ String.prototype.format = function () {
 };
 
 $('button.group').on('click', function() {
-  if ($(this).hasClass('selected')){
+  if ($(this).hasClass('selected')) {
     $(this).removeClass('selected');
   } else {
     $(this).siblings().removeClass('selected');
     $(this).addClass('selected');
   }
 
+  // There is no CHN/TWN N3DS
+  if ($('.region .no_n3ds').hasClass('selected')) {
+    $('#n3ds').removeClass('selected');
+    $('#n3ds').hide();
+  } else {
+    $('#n3ds').show();
+  }
+
+  // Hide/show relevant system versions
+  $('.firmware .group').each(function (index, elem) {
+    var hide_n3ds = $(elem).hasClass('no_n3ds') && $('#n3ds').hasClass('selected');
+    var hide_kct = $(elem).hasClass('no_kct') && $('.kct').hasClass('selected');
+    if (hide_n3ds || hide_kct) {
+      $(elem).removeClass('selected');
+      $(elem).hide();
+    } else {
+      $(elem).show();
+    }
+  })
+
   if (   $('.region').children().hasClass('selected')
       && $('.console').children().hasClass('selected')
-      && $('.firmware').children().hasClass('selected') 
-	) {
+      && $('.firmware').children().hasClass('selected')
+  ) {
     $('#download').addClass('active');
   } else {
     $('#download').removeClass('active');
   }
-  //if (region_selected('div.region'))
-  //$('download')
+
 });
 
 $('#download').on('click', function() {
@@ -35,8 +54,8 @@ $('#download').on('click', function() {
 
   var base = "https://github.com/nedwill/soundhax/raw/master/";
   if(console_ == 'n3ds')
-     var filename = "soundhax-{0}-{1}.m4a".format(region, console_);
+    var filename = "soundhax-{0}-{1}.m4a".format(region, console_);
   else
-	 var filename = "soundhax-{0}-{1}-{2}.m4a".format(region, console_, firmware_);
+    var filename = "soundhax-{0}-{1}-{2}.m4a".format(region, console_, firmware_);
   window.location.href = base + filename;
 });
